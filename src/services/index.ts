5ee059d9b89cafeedffb234624507ac09dfcb701
Lucas Aguiar <luqkinhaas@gmail.com>
2021-12-29 18:46:35 -0300

    update baseurl

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'https://elas-node.herokuapp.com/v1',
-  baseURL: 'http://192.168.0.32:4000/v1',
+  baseURL: 'https://elas-node.herokuapp.com/v1',
 });
